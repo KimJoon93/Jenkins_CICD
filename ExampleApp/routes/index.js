a1de13e465c53a2a913615ae3bacaad984aa1c19
Kim Joon <jk1094@naver.com>
2019-11-02 20:41:51 +0900

    Deploy Test

--- ExampleApp/routes/index.js
+++ ExampleApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Joon Joon Express Deploy' });
+  res.render('index', { title: 'Joon Joon Express Deploy2' });
 });
 
 module.exports = router;
