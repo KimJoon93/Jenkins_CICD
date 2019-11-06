092e05591402e5e72c9a6a21b6eb48c16fcd04e6
Kim Joon <jk1094@naver.com>
2019-11-06 23:41:10 +0900

    Routes with sql

--- ExampleApp/routes/index.js
+++ ExampleApp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Joon Joon Express App deb' });
 });
 
+/* GET DB. */
+router.get('/mysql', function(req, res, next) {
+  res.render('index', { title: 'MYSQL DB' });
+});
+
 module.exports = router;
